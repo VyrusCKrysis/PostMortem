081e8d6179db709c98678a66b85f815cd33119bd
Elias Rodriguez <nexusinfinitus@gmail.com>
2020-02-12 21:16:35 -0500

    Update conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -4,4 +4,6 @@ $conexion = mysqli_connect('localhost', 'id11848820_root', 'bocbestday');
 mysqli_select_db($conexion, 'id11848820_postmortem_bestday');
 $resultado = mysqli_query($conexion, "select IDsesion, fecha_sesion, estatus_sesion from sesiones where estatus_sesion = 1 order by fecha_sesion desc limit 1");
 
+
+//prueba de github
 ?>
